db8d120103d68236caf514d93bda41c44f80c162
CRANK13 <freelancer@ukr.net>
2016-02-18 16:39:09 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Главная</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <link href="style.css" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+   
+      
+   </body>
+</html>
