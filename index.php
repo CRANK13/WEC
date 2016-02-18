c74065337bab910fe42bfc24f77a9181de6d712a
CRANK13 <freelancer@ukr.net>
2016-02-18 18:50:20 +0200

    Add many files

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <title>Главная</title>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <link href="style.css" rel="stylesheet" type="text/css">
+      <title>Главная</title><!-- Название на вкладке -->
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><!-- Обязательно должно присутствовать -->
+      <link href="style.css" rel="stylesheet" type="text/css"><!-- Подключение к файлу сss -->
    </head>
    <body>
-   <table width="690" border="0" align="center" cellpadding="" cellspacing="" bgcolor="#ffffff" class="main_border">
-   	<tr>
-   		<td></td>
-   	</tr>
-   	<tr>
-   		<td></td>
-   	</tr>
-   	<tr>
-   		<td></td>
-   	</tr>
-   </table>
+   <!-- Создается табличка с параметрами -->
+<table width="690" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF" class="main_border">
+	<tr>
+		<td border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF" class="main_border">q</td>
+	</tr>
+	<tr>
+		<td>q</td>
+	</tr>
+	<tr>
+		<td border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF" class="main_border">q</td>
+	</tr>
+</table>  
       
    </body>
 </html>
