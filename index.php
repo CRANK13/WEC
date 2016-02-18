1dfec7a64d70c8df75338b9f28c3faf2f9c3aa56
CRANK13 <freelancer@ukr.net>
2016-02-18 17:12:21 +0200

    Add changes to files

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
       <link href="style.css" rel="stylesheet" type="text/css">
    </head>
    <body>
-   
+   <table width="690" border="0" align="center" cellpadding="" cellspacing="" bgcolor="#ffffff" class="main_border">
+   	<tr>
+   		<td></td>
+   	</tr>
+   	<tr>
+   		<td></td>
+   	</tr>
+   	<tr>
+   		<td></td>
+   	</tr>
+   </table>
       
    </body>
 </html>
