f58745f9ab058d26df91e8ef65fa1102ada7aa5d
CRANK13 <freelancer@ukr.net>
2016-02-19 14:30:23 +0200

    Modified index.php adn create other pages

---
+++ contacts.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>О нас</title><!-- Название на вкладке -->
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><!-- Обязательно должно присутствовать -->
+      <link href="style.css" rel="stylesheet" type="text/css"><!-- Подключение к файлу сss -->
+   </head>
+   <body>
+   <!-- Создается табличка с параметрами -->
+<table width="690px" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF" class="main_border">
+	<tr>
+	<!-- Создается таблца в таблице - "Вложенные таблици" -->
+		<td colspan="2"><img src="img/header.jpg" width="690px" height="100px"></td>
+	</tr>
+	<tr>
+			<td width="182px" valign="top" class="left">
+		<!-- Код на меню навигации -->
+<p align="center" class="title">Навигация</p>
+<div id="coolmenu">
+<a href="index.php">Главная</a>
+<a href="articles.php">Статьи</a>
+<a href="news.php">Новости</a>
+<a href="contacts.php">О нас</a>
+			</td>
+		<td valign="top"><p>Если говорить простым языком – сайт это некоторое количество информации в виде текста, графики и файлов, которые упорядочены с помощью языка разметки HTML и взаимосвязаны между собой ссылками.</p></td>
+	</tr>
+	<tr>
+		<td colspan="2"><img src="img/footer.jpg" width="690px" height="18px"></td>
+	</tr>
+</table>  
+      
+   </body>
+</html>
